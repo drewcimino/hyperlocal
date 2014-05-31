4e1f034b3f9fa9d79b2ac409d47fa3ec270da07e
Drew Cimino <drewcimino@gmail.com>
2014-05-31 15:50:17 -0400

    rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Angelhack::Application.config.secret_key_base = 'ba7b5228d7956e5ed62c9df0e11d20fea014bf3b5f840e01ae02372a58fc30b184e7ae34e28f3eb3fdbb16ba1257993376c2b5af1410ba87411a7fa484d1b69f'
