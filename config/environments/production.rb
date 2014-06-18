4438119c503a3948704950e86cd985b57f4e600b
Drew Cimino <drewcimino@gmail.com>
2014-06-18 01:43:18 -0400

    temporarily serving precompiled assets through rails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,8 @@ Angelhack::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+  # TURNING THIS ON TEMPORARILY UNTIL NGINX GETS CONFIGURED TO DO THIS
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
